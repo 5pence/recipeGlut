677360ea7e060fc9e313104ba90d5a844873f6ae
5pence <sbarriball@me.com>
2018-12-02 18:56:57 +0000

    added login and registration and validation

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, request, session
 from config import Config
-from forms import LoginForm
+from forms import LoginForm, RegisterForm
 from flask_pymongo import PyMongo
+import bcrypt
+
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/recipeGlut'
@@ -10,16 +12,47 @@ app.config.from_object(Config)
 mongo = PyMongo(app)
 
 @app.route('/')
+@app.route('/index')
 def index():
     user = {'username': 'spencer'}
     print(list(mongo.db.recipes.find()))
     return render_template('index.html', title="Home", user=user)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
+    if form.validate_on_submit():
+        users = mongo.db.users
+        login_user = users.find_one({'name': request.form['username']})
+
+        if login_user:
+            if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password']) == login_user['password']:
+                session['username'] = request.form['username']
+                return redirect(url_for('index', title="Sign In", form=form))
+            flash('Invalid username/password combination')
     return render_template("login.html", title="Sign In", form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if form.validate_on_submit():
+        users = mongo.db.users
+        existing_user = users.find_one({'name': request.form['username']})
+
+        if existing_user is None:
+            hash_pass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name': request.form['username'],
+                          'password': hash_pass,
+                          'email': request.form['email']})
+            session['username'] = request.form['username']
+            flash('Thanks for registering')
+            return redirect(url_for('index'))
+        flash('Sorry, that username is already taken - use another')
+        return redirect(url_for('register'))
+    return render_template('register.html', title='Register', form=form)
 
 if __name__ == '__main__':
     app.config['TRAP_BAD_REQUEST_ERRORS'] = True
--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +8,15 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(),
+                                                   Length(min=4, max=20)])
+    password = PasswordField('Password',
+                             validators=[DataRequired(),
+                                         EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('Repeat Password')
+    email = StringField('Email Address', validators=[Length(min=6, max=35), Email()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Register')
